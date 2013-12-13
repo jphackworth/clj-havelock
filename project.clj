a6e4977d2d0c1d035be296cde072a6433aeee2e7
jph <jph@hackworth.be>
2013-12-13 18:56:12 +0800

    initial

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject clj-havelock "0.0.1-SNAPSHOT"
+(defproject clj-havelock "0.0.1"
   :description "API Library for Havelock Investments"
   :url "https://github.com/jphackworth/clj-havelock"
   :license {:name "Mozilla Public License Version 2.0"
             :url "https://www.mozilla.org/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
     [org.clojure/data.json "0.2.3"]
-    [http-kit "2.1.13"]
-    [clj-json "0.5.3"]])
+    [http-kit "2.1.13"]])
--- src/clj_havelock/core.clj
+++ src/clj_havelock/core.clj
@@ -5,75 +5,109 @@
 ; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 (ns clj-havelock.core
-   (:require [clojure.data.json :as json])
-  (:require [org.httpkit.client :as http])
-  (:use [clojure.java.io]))
+ (:require [clojure.data.json :as json]
+  [org.httpkit.client :as http])
+ (:use [clojure.java.io]
+       [clojure.string :only [upper-case lower-case]]))
 
 (def url "https://www.havelockinvestments.com/r")
+(def config (atom {:apikey nil :url nil}))
 
 (defn url-for [cmd] (format "%s/%s" url cmd))
-(defn api-get 
+ 
+(defn post-data 
+  "post-data is used for authenticated, trade api calls.
+
+  It takes an optional map of parameters as an argument.
+
+  You should not use post-data directly, as it requires correctly formatted parameters.
+  "
   [& {:keys [cmd params]
     :or {params nil}}]
-  (def options {:method :post 
-    :content-type "application/json"
-    :user-agent "clj-havelock 0.0.1"
-    :form-params params
-    :insecure? false })
-    @(http/post (url-for cmd) options))
+
+    (let [options {:method :post
+      :content-type "application/json"
+      :user-agent "clj-havelock 0.0.1"
+      :insecure? false
+      :form-params (into {} (filter second params))
+      :keepalive 30000}]
+      (let [response @(http/post (url-for cmd) options)]
+        (case (response :status)
+          200 (json/read-str (response :body) :key-fn keyword)
+          nil))))
+
 
 ; General API 
 ;
 ; General Request assumes unauthenticated request, 
 ; and only optional parameter is symbol
 
-(defn general-request
-  [cmd asset]
-    (if (not (empty? asset))
-      (json/read-str ((api-get :cmd cmd 
-        :params {:symbol (clojure.string/upper-case (name (nth asset 0)))}) :body))
-      (json/read-str ((api-get :cmd cmd) :body))))
-
-(defn get-ticker [& args] (general-request "ticker" args))
-(defn get-ticker-full [& args] (general-request "tickerfull" args))
-(defn get-orderbook [& args] (general-request "orderbook" args))
-(defn get-orderbookfull [& args] (general-request "orderbookfull" args))
-(defn get-dividends [& args] (general-request "dividends" args))
+(defn get-ticker [& {:keys [symbol]}] 
+  (post-data :cmd "ticker" :params {:symbol symbol}))
+(defn get-ticker-full [& {:keys [symbol]}] 
+  (post-data :cmd "tickerfull" :params {:symbol symbol}))
+(defn get-orderbook [& {:keys [symbol]}]
+  (post-data :cmd "orderbook" :params {:symbol symbol}))
+(defn get-orderbookfull [& {:keys [symbol]}]
+  (post-data :cmd "orderbookfull" :params {:symbol symbol}))
+(defn get-dividends [& {:keys [symbol]}]
+  (post-data :cmd "dividends" :params {:symbol symbol}))
 
 ; Account API 
 
-(defn account-request
-  [cmd params]
-  (json/read-str ((api-get :cmd cmd :params params) :body)))
-
-(defn get-portfolio [& {:keys [apikey]}] (account-request "portfolio" {:key apikey}))
-(defn get-orders [& {:keys [apikey]}] (account-request "orders" {:key apikey})) 
-(defn get-balance [& {:keys [apikey]}] (account-request "balance" {:key apikey}))
-(defn deposit-bitcoins [& {:keys [apikey]}] (account-request "deposit" {:key apikey}))
+(defn get-portfolio 
+  [& {:keys [apikey]
+      :or {apikey (@config :apikey)}}]
+  (post-data :cmd "portfolio" :params {:key apikey}))
+(defn get-open-orders
+  [& {:keys [apikey]
+      :or {apikey (@config :apikey)}}] 
+  (post-data :cmd "orders" :params {:key apikey}))
+(defn get-balance
+  [& {:keys [apikey]
+      :or {apikey (@config :apikey)}}]
+  (post-data :cmd "balance" :params {:key apikey}))
+(defn deposit-bitcoins [& {:keys [apikey]
+      :or {apikey (@config :apikey)}}]
+   (post-data :cmd "deposit" :params {:key apikey}))
 
 (defn get-transaction-history
-  [& {:keys [apikey limit sortby sinceid sincets]}]
-  (json/read-str ((api-get :cmd "orders" 
-    :params {:key apikey :limit limit :sort sortby :sinceid sinceid :sincets sincets}) 
-    :body)))
+  [& {:keys [apikey limit sortby sinceid sincets]
+      :or {apikey (@config :apikey)}}]
+  (let [params {:key apikey
+   :limit limit 
+   :sort sortby 
+   :sinceid sinceid 
+   :sincets sincets}]   
+  (post-data :cmd "transactions" :params params)))
 
 (defn withdraw-bitcoins
-  [& {:keys [apikey amount address]}]
-  (json/read-str ((api-get :cmd "withdraw" 
-    :params {:key apikey :amount amount :address address}) 
-    :body)))
+  [& {:keys [apikey amount address] 
+       :or {apikey (@config :apikey)}}]
+  (let [params {:key apikey 
+   :amount amount 
+   :address address}]
+  (post-data :cmd "withdraw" :params params)))
 
 ; Trading API
 
 (defn create-order
-  [& {:keys [apikey asset action price units]}]
-  (json/read-str ((api-get :cmd "ordercreate" 
-    :params {:key apikey :symbol (clojure.string/upper-case (name asset)) :action (clojure.string/lower-case (name action)) :price price :units units}) 
-    :body)))
+  [& {:keys [apikey asset action price units]
+       :or {apikey (@config :apikey)}}]
+  (let [params {:key apikey
+    :symbol (upper-case (name asset)) 
+    :action (lower-case (name action))
+    :price price
+    :units units}]
+  (post-data :cmd "ordercreate" :params params)))
 
 (defn cancel-order
-  [& {:keys [apikey id]}]
-  (json/read-str ((api-get :cmd "ordercancel" 
-    :params {:key apikey :id id}) 
-    :body)))
+  [& {:keys [apikey id] 
+       :or {apikey (@config :apikey)}}]
+  (let [params {:key apikey  
+    :id id}]
+  (post-data :cmd "ordercancel" :params params)))
+
+(defn configure [& {:keys [apikey]}]
+  (if-not (nil? apikey) (swap! config assoc :apikey apikey)))
 
